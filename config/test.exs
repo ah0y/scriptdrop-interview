3cf0d3f6e13dce672d8877c95fa8bd3817f2e6cf
Haile, Aberham <hailea3@nationwide.com>
2019-10-07 09:25:03 -0400

    take 4

--- config/test.exs
+++ config/test.exs
@@ -2,6 +2,22 @@ use Mix.Config
 
 # We don't run a server during test. If one is required,
 # you can enable the server option below.
+
+if System.get_env("DATABASE_URL") do
+  config :scriptdrop, Scriptdrop.Repo,
+         adapter: Ecto.Adapters.Postgres,
+         url: System.get_env("DATABASE_URL"),
+         pool: Ecto.Adapters.SQL.Sandbox
+else
+  # ... Your Local DB Config Here
+  # Configure your database
+  config :scriptdrop, Scriptdrop.Repo,
+         username: "postgres",
+         password: "postgres",
+         database: "scriptdrop_test",
+         hostname: "localhost",
+         pool: Ecto.Adapters.SQL.Sandbox
+end
 config :scriptdrop, ScriptdropWeb.Endpoint,
   http: [port: 4002],
   server: false
@@ -9,10 +25,3 @@ config :scriptdrop, ScriptdropWeb.Endpoint,
 # Print only warnings and errors during test
 config :logger, level: :warn
 
-# Configure your database
-config :scriptdrop, Scriptdrop.Repo,
-  username: "postgres",
-  password: "postgres",
-  database: "scriptdrop_test",
-  hostname: "localhost",
-  pool: Ecto.Adapters.SQL.Sandbox
